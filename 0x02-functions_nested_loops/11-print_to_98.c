3a3dda2116b76d78966feadf9a78af2d1b9d9e65
TiyiM <maluleketiyiselani@outlook.com>
2023-02-25 02:18:32 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,25 @@
-#include <stdio.h>
 #include "main.h"
+#include < stdio.h>
 
 /**
- * print_to_98 - prints all natural numbers from n to 98,and a new line
- * @n: the starting number
+ * print_to_98 - function that prints all natural numbers from n to 98
+ * @n: number to start from and number to print
+ * Return: Always 0.
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	while (n < 9)
 	{
-		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n", n);
+		printf("%i, ", n);
+		n++;
 	}
-	else
+	while (n > 98)
 	{
-		while (n < 98)
-			printf("%d, ", n++);
-		printf("%d\n", n);
+		printf("%i, ", n);
+		n--;
 	}
+	printf("98");
+	_putchar('\n');
+
+	return (0);
 }
