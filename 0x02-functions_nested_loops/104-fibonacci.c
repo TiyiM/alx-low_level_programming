462414f35b66aa6cb45807cfcac3e6a2bdffdb92
TiyiM <maluleketiyiselani@outlook.com>
2023-02-26 09:30:33 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,7 +12,7 @@ int main(void)
 	unsigned long int i;
 	unsigned long int before = 1;
 	unsigned long int after = 2;
-	unsigned long int n = 1000000000;
+	unsigned long int u = 1000000000;
 	unsigned long int before1;
 	unsigned long int before2;
 	unsigned long int after1;
