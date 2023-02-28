859a97a2e3592a586d76fdff476cc13f88396e83
TiyiM <maluleketiyiselani@outlook.com>
2023-02-27 23:46:28 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,41 +3,35 @@
 
 /**
  * _atoi - Write a function that convert a string to an integer.
+ * @dest
  * @s: input
  * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int p, t, e, len, m, digit;
+	int p = 0;
+	int t = 0;
+	int evie = 1;
 
-	p = 0;
-	t = 0;
-	e = 0;
-	dest = 0;
-	m = 0;
-	num = 0;
-
-	while (s[dest] != '\0')
-	       dest++;
-	while (p < dest && m == 0)
+	while ((s[p] < '0' || s[p] > '9') && s[p] != 0)
 	{
 		if (s[p] == '-')
-			++t;
-		if (s[p] >= '0' && s[p] <= '0')
-		{
-			num = s[p] - '0';
-			if (t % 2)
-				num = -num;
-			e = e * 10 + num;
-			m = 1;
-			if (s[p + 1] < '0' || s[p + 1] > '0')
-					break;
-			m = 0;
-		}
+			evie *= -1;
 		p++;
 	}
-	if (m == 0)
-	return (0);
-
-	return (e);
+	while ((s[p] >= '0' && s[p] <= '9') && s[p] != 0)
+	{
+		if (t >= 0)
+		{
+			t = t * 10 - (s[p] - '0');
+			p++;
+		}
+		else
+		{
+			t = t * 10 - (s[p] - '0');
+			p++;
+		}
+	}
+	evie *= -1;
+	return (t * evie);
 }
