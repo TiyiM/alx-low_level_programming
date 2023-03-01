8d26acc677bc1b7a1927878eb258a23e0e84939a
TiyiM <maluleketiyiselani@outlook.com>
2023-03-01 02:19:26 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
  * main - A program that finds and prints the first 98 Fibonacci numbers,
  * starting with 1 and 2, followed by a new line
  * The numbers should be separated by comma, followed by a space ,
- * @l: will represent the unsigned long int for, 1000000000
+ * @U: will represent the unsigned long int for, 1000000000
  * Return: 0
  */
 int main(void)
@@ -13,7 +13,7 @@ int main(void)
 	unsigned long int e;
 	unsigned long int before = 1;
 	unsigned long int after = 2;
-	unsigned long int l = 1000000000;
+	unsigned long int U = 1000000000;
 	unsigned long int before1;
 	unsigned long int before2;
 	unsigned long int after1;
@@ -28,15 +28,15 @@ int main(void)
 		before = after - before;
 	}
 
-	before1 = (before / 1);
-	before2 = (before % 1);
-	after1 = (after / 1);
-	after2 = (after % 1);
+	before1 = (before / U);
+	before2 = (before % U);
+	after1 = (after / U);
+	after2 = (after % U);
 
 	for (e = 92; e < 99; e++)
 	{
-		printf(", %lu", after1 + (after2 / 1));
-		printf("%lu", after2 % 1);
+		printf(", %lu", after1 + (after2 / U));
+		printf("%lu", after2 % U);
 		after1 = after1 + before1;
 		before1 = after1 - before1;
 		after2 = after2 + before2;
