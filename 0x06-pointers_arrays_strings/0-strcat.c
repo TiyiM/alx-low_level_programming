268d8d110053b55957d5c8ad6202a5cf5cac6ab5
TiyiM <maluleketiyiselani@outlook.com>
2023-03-01 11:44:41 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,19 +5,23 @@
 /**
  * *_strcat - Write a function that concatenates two strings.
  * @dest: Returns a pointer to the resulting string dest
- * @src: appends the src string to the dest string,
+ * @src: links the src string to the dest string, overwriting the terminating
+ * null byte (\0) at the end of dest, and then adds a terminating null byte
  * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int e = -1, p;
 
-	while (dest[index++])
-		dest_len++;
+	for (p = 0; dest[p] != '\0'; p++)
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	do
+	{
+		e++;
+		dest[p] = src[e];
+		p++;
+	}
+	while (src[e] != '\0');
 	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strncat - Write a function that concatenates two strings.
+ * @dest: string to be linked
+ * @src: string to be linked to dest
+ * @n: number of n bytes from src to be linked in a series
+ * Return: A pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int 
