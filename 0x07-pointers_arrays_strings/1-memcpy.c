cdd64b39fba7c687a2f5f3e770f228f97cafe321
TiyiM <maluleketiyiselani@outlook.com>
2023-03-07 17:01:43 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,44 +4,20 @@
 
 /**
  * *_memcpy - Write a function that copies memory area.
- * @dest: the address of memory to copy
- * @src: the size of the memory to copy
+ * @dest: memory where is stored
+ * @src: memory where is copied
  * @n: number of bytes to be copied
- * Return: dest
+ * Return: dest, with n bytes changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int e;
+	int e = 0;
+	int m = n;
 
-	e = 0;
-
-	while (e < src)
+	for (; e < m; e++)
 	{
-		if (e % 10)
-		{
-			printf(" ");
-		}
-		if (!(e % 10) && e)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", dest[e]);
-		e++;
+		dest[e] = src[e];
+		n--;
 	}
-	printf("\n");
-}
-/**
- * main - check the code
- *
- * Return: Always 0
- */
-int main(void)
-{
-	char dest[98] = {0x00};
-
-	*_memcpy(dest, 98);
-	_memcpy(buffer, 0x01, 95);
-	printf("-------------------------------------------------\n");
-	*_memcpy(dest, 98);
-	return (0);
+	return (dest);
 }
