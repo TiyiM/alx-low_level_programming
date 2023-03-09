615969173f03fbe3676931fe527f3325529a16e5
TiyiM <maluleketiyiselani@outlook.com>
2023-03-08 16:47:17 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,17 +5,18 @@
 /**
  * _puts_recursion - Write a function that prints a string,
  *followed by a new line.
- * @s: string
+ * @s: input
  * Return: Always 0.
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+
 	else
 		_putchar('\n');
 }
