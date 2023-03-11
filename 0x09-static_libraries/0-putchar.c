42c1f96ea09cbfc2f9695ac2e3907574efea9765
TiyiM <maluleketiyiselani@outlook.com>
2023-03-10 18:31:03 -0800

    0-putchar.c

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to print
+ * Return: 1 (success), -1 error is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (1, &c, 1);
+}
