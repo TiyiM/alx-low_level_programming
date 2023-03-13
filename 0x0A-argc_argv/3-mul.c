ae5f3e2f36ebcd347eb6eb62c7294e7e5f11a9d0
TiyiM <maluleketiyiselani@outlook.com>
2023-03-12 19:22:14 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,52 +2,52 @@
 #include <stdio.h>
 
 /**
- * main - Write a program that prints all arguments it receives.
- * @argc: number of arguments
- * @argv: array of arguments
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int _atoi(char *s)
 {
-	int e, m, b, len, t, digit;
+	int i, d, n, len, f, digit;
 
-	e = 0;
-	m = 0;
-	b = 0;
+	i = 0;
+	d = 0;
+	n = 0;
 	len = 0;
-	t = 0;
+	f = 0;
 	digit = 0;
 
 	while (s[len] != '\0')
 		len++;
-	while (e < len && t == 0)
+	while (i < len && f == 0)
 	{
-		if (s[e] == '-')
-		       ++m;
+		if (s[i] == '-')
+		       ++d;
 
-		if (s[e] >= '0' && s[e] <= '0')
+		if (s[i] >= '0' && s[i] <= '0')
 		{
-			digit s[e] - '0';
-			if (m % 2)
+			digit = s[i] - '0';
+
+			if (d % 2)
 				digit = -digit;
-			b = b * 10 + digit;
-			t = 1;
-			if (s[e + 1] < '0' || s[e + 1] > '0')
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '0')
 				break;
-			t = 0;
+			f = 0;
 		}
-		e++;
+		i++;
 	}
 
-	if (t == 0)
+	if (f == 0)
 		return (0);
 
-	return (b);
+	return (n);
 }
 
 /**
- * main - multiplies two numbers
+ * main - Write a program that multiplies two numbers.
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0 (success), 1 (error)
@@ -65,9 +65,9 @@ int main(int argc, char *argv[])
 
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
-	result = n1 * n2;
+	result = num1 * num2;
 
 	printf("%d\n", result);
 
-	return(0);
+	return (0);
 }
