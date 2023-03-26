f80d6dd6dff1be094c2f2ba78a48ad92ca8d2799
TiyiM <maluleketiyiselani@outlook.com>
2023-03-25 19:33:39 -0700

    2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,20 +12,20 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-        int e;
+	int e;
 
-        e = 0;
-        while (e < n && (src[e]) != '\0')
-        {
-                dest[e] = src[e];
-                e++;
-        }
-        while (e < n)
-        {
-                dest[e] = '\0';
-                e++;
-        }
+	e = 0;
+	while (e < n && (src[e]) != '\0')
+	{
+		dest[e] = src[e];
+		e++;
+	}
+	while (e < n)
+	{
+		dest[e] = '\0';
+		e++;
+	}
 
-        return (dest);
+	return (dest);
 }
 
