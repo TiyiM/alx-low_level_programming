fa1c3a9dab400b7b9ff836f3a73cb2c4c176d52f
TiyiM <maluleketiyiselani@outlook.com>
2023-03-25 19:10:33 -0700

    1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * *_memcpy - Write a function that copies memory area.
+ * *_memcpy - Write a function that copies memory area
  * @dest: memory where is stored
  * @src: memory where is copied
  * @n: number of bytes to be copied
@@ -23,4 +22,3 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	}
 	return (dest);
 }
-
